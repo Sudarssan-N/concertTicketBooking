a51d688259e79ddb57781acf85be52c64f798030
Sudarssan.N <sudarssan73@gmail.com>
2022-09-15 09:11:54 +0530

    error in movirPage.js

--- src/components/movies.js
+++ src/components/movies.js
@@ -1,21 +1,23 @@
 import React from 'react'
 import './movies.css'
+import MoviePage from './MoviePage';
 
 const Movie = ( { data } ) =>{             
-    // const clickHandler = () =>{
-    //   alert("helo world ");
-    // }
+    const clickHandler = () =>{
+      <MoviePage/>
+      alert("helo world ");
+    }
     const{img,title,direc,Duration}=data;
     return (
     <div className='movie' onMouseOver={()=>{
       console.log(title);
     }}>
          <img className="j1" src={img} alt='err' height={100} width={75}/>
-         {/* <h1 className='title'>{title}</h1>
+         <h1 className='title'>{title}</h1>
          <h2>{direc}</h2>
          <h3>{Duration}</h3>
-         <button type='button' className='button1' onClick={clickHandler}>Book</button>  */}
-    </div>
+         <button type='button' className='button1' onClick={clickHandler}>Book</button> 
+    </div> 
   )
 };
 
